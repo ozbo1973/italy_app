d8bbbc6c82bc97cdb0dcafaddc836c0089ab12c5
Brady Bovero <bbbove20@gmail.com>
2019-09-15 20:03:57 -0600

    Added Yelp section.

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,5 +1,16 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Typography } from "@material-ui/core";
+import {
+  Paper,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  CardHeader,
+  Divider
+} from "@material-ui/core";
+import Rain from "@material-ui/icons/BeachAccess";
+import ClearDay from "@material-ui/icons/WbSunny";
+import Cloudy from "@material-ui/icons/WbCloudy";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -7,17 +18,84 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const weatherData = {
+  timezone: "Italy/Rome",
+  currently: {
+    time: 1509993277,
+    summary: "Drizzle",
+    temperature: 66.1,
+    icon: "clear-day",
+    cloudCover: 0.7,
+    visibility: 9.84
+  },
+  daily: {
+    summary: "Rain starting in afternoon,continue until the evening",
+    data: [
+      {
+        summary: "Clearing in the morning and evening",
+        temperatureHigh: 66.35,
+        temperatureLow: 41.28,
+        icon: "rain"
+      },
+      {
+        summary: "Cloudy in the evening",
+        temperatureHigh: 72.35,
+        temperatureLow: 52.28,
+        icon: "clear-day"
+      },
+      {
+        summary: "Cloudy most of the day",
+        temperatureHigh: 65.35,
+        temperatureLow: 52.28,
+        icon: "cloudy"
+      }
+    ]
+  }
+};
+
+const weatherIcons = {
+  rain: <Rain />,
+  clearday: <ClearDay />,
+  cloudy: <Cloudy />
+};
+
 const Weather = ({ location }) => {
   const classes = useStyles();
+  const { currently, daily } = weatherData;
+  const getIcon = icon => weatherIcons[icon.split("-").join("")];
+
   return (
     <Paper className={classes.root}>
-      <Typography variant="h5" component="span">
-        Weather
-      </Typography>
-      <Typography variant="h5" component="span">
-        75 &deg;
-      </Typography>
-      <Typography variant="body2">Sunny with no chance of rain.</Typography>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h5">
+            Weather {getIcon(currently.icon)}
+          </Typography>
+
+          <Typography variant="h6" component="h6">
+            Temperature: {currently.temperature}
+          </Typography>
+          <Typography variant="body2">Summary: {currently.summary}</Typography>
+          <Typography variant="body2">{`Cloud Cover: ${currently.cloudCover}`}</Typography>
+          <Divider />
+          <Typography variant="body2">
+            <span>Tomorrow:</span> {getIcon(daily.data[1].icon)}
+            <span>
+              H:{daily.data[1].temperatureHigh}
+              L:{daily.data[1].temperatureLow}
+            </span>
+            <div>{daily.data[1].summary}</div>
+          </Typography>
+          <Typography variant="body2">
+            <span>Next Day:</span> {getIcon(daily.data[1].icon)}
+            <span>
+              H:{daily.data[2].temperatureHigh}
+              L:{daily.data[2].temperatureLow}
+            </span>
+            <div>{daily.data[2].summary}</div>
+          </Typography>
+        </CardContent>
+      </Card>
     </Paper>
   );
 };
--- src/components/yelp.js
+++ src/components/yelp.js
@@ -1,39 +1,254 @@
-import useStyles from "../../static/styles/dataTable.style";
-import {
-  Paper,
-  Typography,
-  Table,
-  TableHead,
-  TableCell,
-  TableBody,
-  TableRow
-} from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.secondary.main,
+    padding: theme.spacing(1),
+    borderRadius: "5px"
+  },
+  gridList: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: theme.palette.background.paper
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)"
+  },
+  tableWrapper: {
+    overflow: "auto",
+    backgroundColor: theme.palette.secondary.main,
+    padding: theme.spacing(1),
+    borderRadius: "5px"
+  }
+}));
+
+const yelpData = {
+  total: 8228,
+  businesses: [
+    {
+      rating: 4,
+      price: "$",
+      phone: "+14152520800",
+      id: "E8RJkjfdcwgtyoPMjQ_Olg",
+      alias: "four-barrel-coffee-san-francisco",
+      is_closed: false,
+      categories: [
+        {
+          alias: "coffee",
+          title: "Coffee & Tea"
+        }
+      ],
+      review_count: 1738,
+      name: "Four Barrel Coffee",
+      url: "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
+      coordinates: {
+        latitude: 37.7670169511878,
+        longitude: -122.42184275
+      },
+      image_url:
+        "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
+      location: {
+        city: "San Francisco",
+        country: "US",
+        address2: "",
+        address3: "",
+        state: "CA",
+        address1: "375 Valencia St",
+        zip_code: "94103"
+      },
+      distance: 1604.23,
+      transactions: ["pickup", "delivery"]
+    },
+    {
+      rating: 4,
+      price: "$",
+      phone: "+14152520800",
+      id: "E8RJkjfdcwgtyoPMjQ_Olg",
+      alias: "four-barrel-coffee-san-francisco",
+      is_closed: false,
+      categories: [
+        {
+          alias: "coffee",
+          title: "Coffee & Tea"
+        }
+      ],
+      review_count: 1738,
+      name: "Four Barrel Coffee",
+      url: "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
+      coordinates: {
+        latitude: 37.7670169511878,
+        longitude: -122.42184275
+      },
+      image_url:
+        "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
+      location: {
+        city: "San Francisco",
+        country: "US",
+        address2: "",
+        address3: "",
+        state: "CA",
+        address1: "375 Valencia St",
+        zip_code: "94103"
+      },
+      distance: 1604.23,
+      transactions: ["pickup", "delivery"]
+    },
+    {
+      rating: 4,
+      price: "$",
+      phone: "+14152520800",
+      id: "E8RJkjfdcwgtyoPMjQ_Olg",
+      alias: "four-barrel-coffee-san-francisco",
+      is_closed: false,
+      categories: [
+        {
+          alias: "coffee",
+          title: "Coffee & Tea"
+        }
+      ],
+      review_count: 1738,
+      name: "Four Barrel Coffee",
+      url: "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
+      coordinates: {
+        latitude: 37.7670169511878,
+        longitude: -122.42184275
+      },
+      image_url:
+        "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
+      location: {
+        city: "San Francisco",
+        country: "US",
+        address2: "",
+        address3: "",
+        state: "CA",
+        address1: "375 Valencia St",
+        zip_code: "94103"
+      },
+      distance: 1604.23,
+      transactions: ["pickup", "delivery"]
+    },
+    {
+      rating: 4,
+      price: "$",
+      phone: "+14152520800",
+      id: "E8RJkjfdcwgtyoPMjQ_Olg",
+      alias: "four-barrel-coffee-san-francisco",
+      is_closed: false,
+      categories: [
+        {
+          alias: "coffee",
+          title: "Coffee & Tea"
+        }
+      ],
+      review_count: 1738,
+      name: "Four Barrel Coffee",
+      url: "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
+      coordinates: {
+        latitude: 37.7670169511878,
+        longitude: -122.42184275
+      },
+      image_url:
+        "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
+      location: {
+        city: "San Francisco",
+        country: "US",
+        address2: "",
+        address3: "",
+        state: "CA",
+        address1: "375 Valencia St",
+        zip_code: "94103"
+      },
+      distance: 1604.23,
+      transactions: ["pickup", "delivery"]
+    },
+    {
+      rating: 4,
+      price: "$",
+      phone: "+14152520800",
+      id: "E8RJkjfdcwgtyoPMjQ_Olg",
+      alias: "four-barrel-coffee-san-francisco",
+      is_closed: false,
+      categories: [
+        {
+          alias: "coffee",
+          title: "Coffee & Tea"
+        }
+      ],
+      review_count: 1738,
+      name: "Four Barrel Coffee",
+      url: "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
+      coordinates: {
+        latitude: 37.7670169511878,
+        longitude: -122.42184275
+      },
+      image_url:
+        "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
+      location: {
+        city: "San Francisco",
+        country: "US",
+        address2: "",
+        address3: "",
+        state: "CA",
+        address1: "375 Valencia St",
+        zip_code: "94103"
+      },
+      distance: 1604.23,
+      transactions: ["pickup", "delivery"]
+    }
+    // ...
+  ],
+  region: {
+    center: {
+      latitude: 37.767413217936834,
+      longitude: -122.42820739746094
+    }
+  }
+};
 
 const Yelp = () => {
   const classes = useStyles();
   return (
-    <Paper className={`${classes.root} ${classes.tableWrapper}`}>
-      <div className={classes.regularTable}>
-        <Typography variant="h6" className={classes.regularTableTitle}>
-          Places to Eat(yelp)
-        </Typography>
-        <Table>
-          <TableHead>
-            <TableCell>Title</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>View</TableCell>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>Mario's Pizza Joint</TableCell>
-              <TableCell>
-                A delicious place to eat for everyone who enjoys pizza
-              </TableCell>
-              <TableCell>view full</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </div>
+    <Paper className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+          <ListSubheader component="div">Places to Eat (Yelp)</ListSubheader>
+        </GridListTile>
+        {yelpData.businesses.map(tile => (
+          <GridListTile key={tile.id}>
+            <img src={tile.image_url} alt={tile.name} />
+            <GridListTileBar
+              title={tile.name}
+              subtitle={
+                <div>
+                  <span>rating: {tile.rating}</span>{" "}
+                  <span>reviews: {tile.review_count}</span>
+                </div>
+              }
+              actionIcon={
+                <IconButton
+                  aria-label={`info about ${tile.name}`}
+                  className={classes.icon}
+                  onClick={() => alert(tile.review_count)}
+                >
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
     </Paper>
   );
 };
